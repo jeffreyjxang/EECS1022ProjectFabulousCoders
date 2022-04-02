e20c540e7d1348406ec95a6c905f88458b1b2f21
Jeffrey Jiang <ouraize2003@gmail.com>
2022-04-02 13:28:10 -0400

    game works

--- app/src/main/java/com/example/finalprojectnewtesting/gameplayView.java
+++ app/src/main/java/com/example/finalprojectnewtesting/gameplayView.java
@@ -29,7 +29,7 @@ public class gameplayView extends AppCompatActivity {
 
         Utilities util = new Utilities();
 
-        if (Integer.parseInt(mode) == 1) {
+        if (Integer.parseInt(mode) == 2) {
             System.out.println(mode + attempts);
             TextView attemptBox = (TextView) findViewById(R.id.attempts);
             TextView score = (TextView) findViewById(R.id.score);
@@ -74,43 +74,29 @@ public class gameplayView extends AppCompatActivity {
                 startActivity(i);
             }
         } else {
-            System.out.println(mode + attempts);
+            EditText userGuessBox = (EditText) findViewById(R.id.userAttemptNum);
+            int userGuess = Integer.parseInt(userGuessBox.getText().toString());
+
             TextView attemptBox = (TextView) findViewById(R.id.attempts);
             TextView score = (TextView) findViewById(R.id.score);
 
-            if (attemptBox.getText().toString().equals("") || attemptBox.getText().toString().equals(null)) {
-                attemptBox.setText("0");
-            } else {
-
-                attemptBox.setText("" + (Integer.parseInt(attemptBox.getText().toString()) + 1));
-            }
-
-            if (Integer.parseInt(attemptBox.getText().toString()) < Integer.parseInt(attempts)) {
-                EditText userGuessBox = (EditText) findViewById(R.id.userAttemptNum);
-                int userGuess = Integer.parseInt(userGuessBox.getText().toString());
-
-                int number = util.randNumGen(0);
-                System.out.println(number);
-                if (number == (userGuess)) {
+            int number = util.randNumGen(0);
+            System.out.println(number);
+            if (number == (userGuess)) {
                     //win popup
-                    userGuessBox.setText("");
-                    if (score.getText().toString().equals("") || score.getText().toString().equals(null)) {
-                        attemptBox.setText("0");
-                    } else {
-
-                        score.setText("" + (Integer.parseInt(score.getText().toString()) + 1));
-                    }
+                userGuessBox.setText("");
+                if (score.getText().toString().equals("") || score.getText().toString().equals(null)) {
+                    attemptBox.setText("0");
                 } else {
-                    //lose popup
 
-                    Intent i = new Intent(this, gameMain.class);
-                    startActivity(i);
+                    score.setText("" + (Integer.parseInt(score.getText().toString()) + 1));
                 }
             } else {
+                //lose popup
+
                 Intent i = new Intent(this, gameMain.class);
                 startActivity(i);
             }
         }
-
     }
 }
